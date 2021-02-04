cf172184a8a8e527d05ba478066fa09c7c7be03b
unknown <luck2901@naver.com>
2021-02-04 20:31:35 +0900

    Update

--- App.js
+++ App.js
@@ -2,17 +2,27 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Loading from "./Loading";
 import {Alert} from "react-native";
+import axios from "axios";
+
+const API_KEY = "d6ee5391a7da7b3a908bb7093fd3fa0d";
 
 export default class extends React.Component {
   state = {
     isLoading: true
-  }
+  };
+  getWeather = async(latitude, longitude) =>{
+    const {data} = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+      );
+      console.log(data);
+  };
   getLocation = async() =>{
     try{
       await Location.requestPermissionsAsync();
-      const {coords : {latitude, longtinude}} = await Location.getCurrentPositionAsync();
+      const {coords : {latitude, longitude}} = await Location.getCurrentPositionAsync();
       //send to API and get weather!
-      this.setState({isLoading:false})
+      this.getWeather(latitude, longitude);
+      this.setState({isLoading:false});
     }catch(error){
       Alert.alert("Can't find you.", "so sad");
     }
